c96a06be8d65d3d8853d055ac882148170ced412
Tutorial <foditoon1@gmail.com>
2025-08-07 17:14:36 +0400

    Add profile and registration system

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.index, name='login'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', views.CustomLoginView.as_view(redirect_authenticated_user=True, template_name='registration/login.html'),
+         name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('profile/', views.profile, name='users-profile'),
+path('password_change/', views.ChangePasswordView.as_view(), name='password_change'),
 ]
