41b0cad3c4d0485db9eb3455f47cee0cd0089aaf
Tutorial <foditoon1@gmail.com>
2025-08-07 13:23:18 +0400

    uh forgot to make new branch

--- core/urls.py
+++ core/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('menu/', include('products.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
