298174c78dda10a4e237fa0b1bbf655fd2768353
Tutorial <foditoon1@gmail.com>
2025-08-18 21:30:54 +0400

    add url

--- orders/urls.py
+++ orders/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:order_id>/', views.order_detail, name='order_detail'),
     path('', views.order_list, name='order_list'),
     path('api/bot/<int:order_id>/<str:action>/', api.bot_update_status, name='bot_update_status'),
+    path('payment/<int:order_id>/', views.payment_callback, name='payment_callback'),
 ]
